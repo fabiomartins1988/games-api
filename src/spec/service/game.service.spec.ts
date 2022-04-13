6898ed1d463e0b30d633420405ed38e5fef1658a
Fabio Martins Silva <CIANDT\fmsilva@lnb035412cps.ciandt.global>
2022-04-13 12:05:02 -0300

    add: test scenario get all games

--- src/spec/service/game.service.spec.ts
+++ src/spec/service/game.service.spec.ts
@@ -4,8 +4,20 @@ import { Request, Response } from 'express';
 describe('When getAllGames is called', () => {
   const req = {} as Request;
   const res = {} as Response;
-  res.json = jest.fn().mockReturnValue({"hello": "world"});
-  it('Then the return is "hello" : "world" json', () => {
+  res.status = () => res;
+  res.json = () => res;
+
+  xit('Then return all games', () => {
+    const database = { games: [{
+        id: 0,
+        name: "some",
+        plataform: "some",
+        price: 120
+      }
+    ]}
+
+    jest.mock('../../main/database/db.json', () => {database})
+
     expect(gameService.getAllGames(req, res)).toStrictEqual({"hello": "world"});
   });
 });
